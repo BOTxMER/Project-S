b457bf7246a2fa37f8f5212957bd04fa315c2b9c
Ваня <divan-19@mail.ru>
2025-04-08 00:24:00 +0500

    3 weeek

--- 3 week/лалала/Car/DTOs/CreateCarDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace CarApp.DTOs
-{
-    public class CreateCarDTO
-    {
-        public string? Model { get; set; }
-        public string? Description { get; set; }
-        public double? Price { get; set; }
-    }
-}
--- 3 week/лалала/Car/DTOs/UpdateCarDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace CarApp.DTOs
-{
-    public class UpdateCarDTO
-    {
-        public string? Model { get; set; }
-        public string? Description { get; set; }
-        public double? Price { get; set; }
-    }
-}
